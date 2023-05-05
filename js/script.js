b9ac22c9b0d6077512a44f6fe38512d8f7b20069
Jean Silva <jeanpsilva10@gmail.com>
2023-05-05 10:05:36 -0300

    [feat] tema muda de acordo com a hora do dia

--- js/script.js
+++ js/script.js
@@ -7,9 +7,9 @@ window.addEventListener('scroll', function(){
 })
 
 const date = new Date();
-const hours = date.getHours()
-const minutes = date.getMinutes()
-if(hours<6 || hours>=18){
+const hours = date.getHours();
+const minutes = date.getMinutes();
+if(hours<=5 && minutes<=59|| hours>=17 && minutes <=59){
     backgroundHome.style.backgroundImage = "url('../img/background2.jpg')"
     body.style.backgroundColor = '#0c1220';
     window.addEventListener('scroll', function(){
