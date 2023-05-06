fb61189ed30a303b993d898aea44cf0cce8d64ea
Jean Silva <jeanpsilva10@gmail.com>
2023-05-05 22:20:36 -0300

    [fix]

--- js/script.js
+++ js/script.js
@@ -1,11 +1,6 @@
 const nav = document.querySelector('nav')
 const backgroundHome = document.querySelector('#background')
 const body = document.querySelector('body')
-
-window.addEventListener('scroll', function(){
-    nav.classList.toggle('scroll', window.scrollY>0)
-})
-
 const date = new Date();
 const hours = date.getHours();
 const minutes = date.getMinutes();
@@ -14,5 +9,9 @@ if((hours<=5 && minutes<=59) || (hours>=17 && minutes <=59)){
     body.style.backgroundColor = '#0c1220';
     window.addEventListener('scroll', function(){
         nav.classList.toggle('scrolldark', window.scrollY>0)
+    })
+}else{
+    window.addEventListener('scroll', function(){
+        nav.classList.toggle('scroll', window.scrollY>0)
     })
 }
