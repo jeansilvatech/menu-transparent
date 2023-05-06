1217793fbd5e2a59e70a1cd080bfa5744ca32ba5
Jean Silva <jeanpsilva10@gmail.com>
2023-05-05 22:16:49 -0300

    [fix] correção de bug no background

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ window.addEventListener('scroll', function(){
 const date = new Date();
 const hours = date.getHours();
 const minutes = date.getMinutes();
-if(hours<=5 && minutes<=59|| hours>=17 && minutes <=59){
+if((hours<=5 && minutes<=59) || (hours>=17 && minutes <=59)){
     backgroundHome.style.backgroundImage = "url('../img/background2.jpg')"
     body.style.backgroundColor = '#0c1220';
     window.addEventListener('scroll', function(){
